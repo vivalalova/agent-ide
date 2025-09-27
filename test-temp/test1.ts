51bb710d8bbe43623175928631a4bbfb2d6c5130
Lova Shih <vivalalova0@gmail.com>
2025-09-27 15:19:17 +0800

    remove: 清理測試暫存檔案

--- test-temp/test1.ts
+++
@@ -1,9 +0,0 @@
-export function getUserById(id: number): User {
-  return database.findUser(id);
-}
-
-export class UserService {
-  async fetchUser(userId: number) {
-    return await this.api.get(`/users/${userId}`);
-  }
-}
--- test-temp/test2.js
+++
@@ -1,8 +0,0 @@
-const userData = {
-  name: 'John',
-  age: 30
-};
-
-function processUser(user) {
-  console.log('Processing user:', user.name);
-}
